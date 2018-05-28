c2e8f9b1374298113f09739678132da8c162e639
Christian Schulze <cschulze@electronicpartner.de>
2018-05-28 12:11:21 +0200

    erweiterung

--- Sudoku/Sudoku/menu.h
+++ Sudoku/Sudoku/menu.h
@@ -2,25 +2,41 @@
 
 void menu(void)
 {
+	int imenu;
 	int iError = 0;
 
 	do
 	{
 		printf("(1) Schnellstart\n\n");
-		printf("(2) Login\n");
-		printf("(3) Benutzer anlegen\n\n");
-		printf("(4) Bestenliste\n\n");
-		printf("(5) Regeln anzeigen\n\n");
-		printf("(6) Beenden\n\n");
+		printf("(2) Neues Spiel starten\n\n");
+		printf("(3) Login\n");
+		printf("(4) Benutzer anlegen\n\n");
+		printf("(5) Bestenliste\n\n");
+		printf("(6) Regeln anzeigen\n\n");
+		printf("(7) Beenden\n\n");
 		fflush(stdin);
 		iError = scanf("%i", &imenu);
-	} while (iError == 0 || imenu < 1 || imenu > 2);
+	} while (iError == 0 || imenu < 1 || imenu > 7);
 
 	printf("\n");
 
+	switch (imenu)
+	{
+	case constant1:
+		statements
+			break;
+
+	case constant2:
+		statements
+			break;
+
+	case constant3:
+		statements
+			break;
 
 
 
 	system("Pause");
 
 }
+
