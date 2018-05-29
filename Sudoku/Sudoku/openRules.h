2a465767f2b55dd78f9b9f5e48e0ada75f0e1a2e
Christian Schulze <cschulze@electronicpartner.de>
2018-05-29 08:15:13 +0200

    rules geändert

--- Sudoku/Sudoku/openRules.h
+++ Sudoku/Sudoku/openRules.h
@@ -1,11 +1,9 @@
-#include <stdio.h>;
-#include <stdlib.h>;
-#include "windows.h";
+#include "include.h"
 
 void openFile(void)
 {
 	// opens a pdf file
-	char file[100] = "C://Users/iaBag/source/repos/SudokuProject/Sudoku/Sudoku/resouces/pdf.pdf";
+	char file[100] = "/sudoku-anleitung.pdf";
 	ShellExecute(NULL, NULL, file, NULL, NULL, SW_SHOW);
 
 	return 0;
