895bfdc773d3179e76c57052c96047a7441c9f8c
Christian Schulze <cschulze@electronicpartner.de>
2018-06-05 11:49:36 +0200

    neu

--- Sudoku/Sudoku/spiel.c
+++ Sudoku/Sudoku/spiel.c
@@ -8,7 +8,7 @@ void menuImSpiel(void)
 }
 
 
-void spielFeldAusgabe(void) {
+int spielFeldAusgabe(void) {
 
 	// Definition
 	char cZeile = 0;
@@ -18,122 +18,143 @@ void spielFeldAusgabe(void) {
 	int iZeile = 0;
 	int iSpalte = 0;
 	int iError;
-
-	/*do
-	{*/
-
-	//Layout
-	system("cls");
-	sudoku_header();
-	layout();
-
-	//system("\t");
-	//system("Pause");
-
+	int iEndlos = 0;
 
 	do
 	{
-		/*if (iZeile < 0 && iZeile > 8 && iZeile != NULL)
+		
+
+
+		//Layout
+		system("cls");
+		sudoku_header();
+		layout();
+
+		do
 		{
-		printf("Ihre Zahlen dürfen nur von 1 - 9 gehen!\n");
-		}*/
-		printf("\tZeile: ");
+			printf("\tZeile: ");
+			fflush(stdin);
+			while (getchar() != '\n');
+			iError = scanf("%c", &cZeile);
+			if (cZeile == 'x')
+			{
+				menu();
+				iEndlos = 1;
+			}
+			if (cZeile == 'n')
+			{
+				sudokuGenShift();
+				spielFeldAusgabe();
+				iEndlos = 1;
+			}
+			if (cZeile == 'l')
+			{
+				lösungAnzeigen();
+				iEndlos = 1;
+			}
+			if (iError != 0)
+			{
+				iZeile = cZeile - '0';
+			}
+		} while (iError == 0 || iZeile == -38);
 		fflush(stdin);
-		while (getchar() != '\n');
-		iError = scanf("%c", &cZeile);
-		if (cZeile == 'x')
-		{
-			menu();
-		}
-		if (cZeile == 'n')
-		{
-			sudokuGenShift();
-			spielFeldAusgabe();
-		}
-		if (cZeile == 'l')
-		{
-			
-		}
-		if (iError != 0)
-		{
-			iZeile = cZeile - '0';
-		}
-	} while (iError == 0 || iZeile == -38);
-	
-
-
-	do
-	{
-		/*if (iSpalte < 0 && iSpalte > 8 && iSpalte != NULL)
-		{
-		printf("Ihre Zahlen dürfen nur von 1 - 9 gehen!\n");
-		}*/
-		printf("\tSpalte: ");
-		fflush(stdin);
-		while (getchar() != '\n');
-		iError = scanf("%c", &cSpalte);
-		if (cSpalte == 'x')
-		{
-			menu();
-		}
-		if (cSpalte == 'n')
-		{
-			sudokuGenShift();
-			spielFeldAusgabe();
-		}
-		if (cSpalte == 'l')
+		if (iEndlos != 1)
 		{
 
-		}
-		if (iError != 0)
-		{
-			iSpalte = cSpalte - '0';
-		}
-	} while (iError == 0 || iSpalte == -38);
-	
-
-	do
-	{
-		/*if (iZahl < 0 && iZahl > 8 && iZahl != NULL)
-		{
-		printf("Ihre Zahlen dürfen nur von 1 - 9 gehen!\n");
-		}*/
-		printf("\tZahl: ");
-		fflush(stdin);
-		while (getchar() != '\n');
-		iError = scanf("%2c", &cZahl);
-		if (cZahl == 'x')
-		{
-			menu();
-		}
-		if (cZahl == 'n')
-		{
-			sudokuGenShift();
-			spielFeldAusgabe();
-		}
-		if (cZahl == 'l')
-		{
-
-		}
-		if (iError != 0)
-		{
-			iZahl = cZahl - '0';
-		}
-	} while (iError == 0 || iZahl == -38);
-
-
-	iGrid[iZeile - 1][iSpalte - 1] = iZahl;
 
 
 
-	spielFeldAusgabe();
-	
+			do
+			{
+				printf("\tSpalte: ");
+				while (getchar() != '\n');
+				iError = scanf("%c", &cSpalte);
+				if (cSpalte == 'x')
+				{
+					menu();
+				}
+				if (cSpalte == 'n')
+				{
+					sudokuGenShift();
+					spielFeldAusgabe();
+				}
+				if (cSpalte == 'l')
+				{
+					lösungAnzeigen();
+				}
+				if (iError != 0)
+				{
+					iSpalte = cSpalte - '0';
+				}
+			} while (iError == 0 || iSpalte == -38);
+			fflush(stdin);
+		}
+		if (iEndlos != 1)
+		{
+			 
+			do
+			{
+				printf("\tZahl: ");
+				//fflush(stdin);
+				while (getchar() != '\n');
+				iError = scanf("%2c", &cZahl);
+				if (cZahl == 'x')
+				{
+					menu();
+				}
+				if (cZahl == 'n')
+				{
+					sudokuGenShift();
+					spielFeldAusgabe();
+				}
+				if (cZahl == 'l')
+				{
+					lösungAnzeigen();
+				}
+				if (iError != 0)
+				{
+					iZahl = cZahl - '0';
+				}
+			} while (iError == 0 || iZahl == -38);
+			fflush(stdin);
 
 
+			iGrid[iZeile - 1][iSpalte - 1] = iZahl;
+		}
+
+	}while(iEndlos == 0);
+	return 0;
 }
 
 void lösungAnzeigen(void)
 {
+	// coping the board into iGridCopy
+	for (int iIndexV = 0; iIndexV < 9; iIndexV++)
+	{
+		for (int iIndexH = 0; iIndexH < 9; iIndexH++)
+		{
+			iGrid[iIndexV][iIndexH] = iGridCopy[iIndexV][iIndexH];
+		}
+	}
+	//Layout
+	system("cls");
+	sudoku_header();
+	layout();
+	printf("\tSPIEL VORBEI! \n\tLoesung wurde angezeigt");
+}
 
+int sudokuAbfrage(void)
+{
+	for (int iIndexV = 0; iIndexV < 9; iIndexV++)
+	{
+		for (int iIndexH = 0; iIndexH < 9; iIndexH++)
+		{
+			if (iGrid[iIndexV][iIndexH] != iGridCopy[iIndexV][iIndexH])
+			{
+				return 0; // Falsch
+			}
+		}
+	}
+	return 1; // Richtig
 }
 
